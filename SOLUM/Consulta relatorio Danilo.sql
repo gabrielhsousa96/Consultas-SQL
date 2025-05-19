6e2c29c0888de064bde0b66dcbc52420411f603b
gabrielhsousa96 <gabrielh.sousaf96@gmail.com>
2025-05-19 15:34:45 -0300

    Update Old Not 19-05
    Update Old Not 19-05

---
+++ SOLUM/Consulta relatorio Danilo.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{2D5DD9CA-C9F7-411D-A01D-6DBC898FC08E}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {4B69C580-A206-4CB7-96D4-BA63314363EC}
+	EndGlobalSection
+EndGlobal
